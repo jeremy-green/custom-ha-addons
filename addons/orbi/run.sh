ce8c245f234f53778c1b68a765dd80bf9e2b5761
Jeremy Green <hello@jeremy.green>
2020-07-30 13:21:53 -0400

    dunno

--- addons/orbi/run.sh
+++ addons/orbi/run.sh
@@ -6,6 +6,9 @@ CONFIG_PATH=/data/options.json
 ROUTER_USER=$(jq --raw-output ".router_user" $CONFIG_PATH)
 ROUTER_PASS=$(jq --raw-output ".router_pass" $CONFIG_PATH)
 
+TARGET_ROUTER_USER="$(bashio::config 'router_user')"
+TARGET_ROUTER_PASS="$(bashio::config 'router_pass')"
+
 env
 
 node -v
