6f85237180f7fb664730380c531eb35258dda3fa
Jeremy Green <hello@jeremy.green>
2020-07-30 18:53:27 -0400

    move env setting

--- addons/orbi/run.sh
+++ addons/orbi/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-CONFIG_PATH=/data/options.json
 
 env
 
@@ -10,6 +9,7 @@ npm -v
 
 ls -la
 
+CONFIG_PATH=/data/options.json \
 ROUTER_USER=$(jq --raw-output ".router_user" $CONFIG_PATH) \
 ROUTER_PASS=$(jq --raw-output ".router_pass" $CONFIG_PATH) \
 npm run start
