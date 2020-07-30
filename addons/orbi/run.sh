e6c6143a71386afc164db768c3a95993222d5ab9
Jeremy Green <hello@jeremy.green>
2020-07-30 13:43:06 -0400

    update dockerfie

--- addons/orbi/run.sh
+++ addons/orbi/run.sh
@@ -3,11 +3,8 @@ set -e
 
 CONFIG_PATH=/data/options.json
 
-ROUTER_USER=$(jq --raw-output ".router_user" $CONFIG_PATH)
-ROUTER_PASS=$(jq --raw-output ".router_pass" $CONFIG_PATH)
-
-TARGET_ROUTER_USER="$(bashio::config 'router_user')"
-TARGET_ROUTER_PASS="$(bashio::config 'router_pass')"
+ROUTER_USER="$(bashio::config 'router_user')"
+ROUTER_PASS="$(bashio::config 'router_pass')"
 
 env
 
